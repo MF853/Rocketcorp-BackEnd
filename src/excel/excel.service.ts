34836fc80cbf27fc9ae1fc5120f425eb0fec7ab5
Erico <erico.chen@hotmail.com>
2025-07-05 15:33:29 -0300

    [feat]: adicionar a funcao de importar avaliacao360

--- src/excel/excel.service.ts
+++ src/excel/excel.service.ts
@@ -3,10 +3,12 @@ import * as ExcelJS from 'exceljs';
 import { UsersService } from '../users/users.service';
 import { ReferenciaService } from 'src/referencia/referencia.service';
 import { CicleService } from 'src/cicle/cicle.service';
+import { AvaliacaoService } from 'src/avaliacao/avaliacao.service';
+import { MotivacaoTrabalhoNovamente } from '../avaliacao/dto/create-avaliacao.dto';;
 
 @Injectable()
 export class ExcelService {
-  constructor(private readonly usersService: UsersService, private readonly referenciaService: ReferenciaService, private readonly cicleService: CicleService) {}
+  constructor(private readonly usersService: UsersService, private readonly referenciaService: ReferenciaService, private readonly cicleService: CicleService, private readonly avaliacao360Service: AvaliacaoService) {}
 
   // async processExcel(filePath: string) {
   async processExcel(buffer: Buffer) {
@@ -29,11 +31,14 @@ export class ExcelService {
     }
 
     // if (autoSheet) await this.importAutoavaliacao(autoSheet);
-    // if (avaliacao360Sheet) await this.importAvaliacao360(avaliacao360Sheet);
-
-    if (referenciasSheet && userId !== undefined && cicleId !== undefined) {
-      await this.importReferencias(referenciasSheet, userId, cicleId);
+   
+    if (avaliacao360Sheet && userId !== undefined && cicleId !== undefined) {
+      await this.importAvaliacao360(avaliacao360Sheet, userId, cicleId);
     }
+
+    // if (referenciasSheet && userId !== undefined && cicleId !== undefined) {
+    //   await this.importReferencias(referenciasSheet, userId, cicleId);
+    // }
 
     return { message: 'Importação concluída com sucesso' };
   }
@@ -87,8 +92,47 @@ export class ExcelService {
     // Implementar lógica para importar autoavaliações
   }
 
-  async importAvaliacao360(sheet: ExcelJS.Worksheet) {
-    // Implementar lógica para importar avaliações 360
+  async importAvaliacao360(sheet: ExcelJS.Worksheet, userId: number, cicleId: number) {
+
+    const motivacaoMap: Record<string, MotivacaoTrabalhoNovamente> = {
+      'Discordo Totalmente': MotivacaoTrabalhoNovamente.DISCORDO_TOTALMENTE,
+      'Discordo Parcialmente': MotivacaoTrabalhoNovamente.DISCORDO_PARCIALMENTE,
+      'Indiferente': MotivacaoTrabalhoNovamente.INDIFERENTE,
+      'Concordo Parcialmente': MotivacaoTrabalhoNovamente.CONCORDO_PARCIALMENTE,
+      'Concordo Totalmente': MotivacaoTrabalhoNovamente.CONCORDO_TOTALMENTE,
+    };
+
+    sheet.eachRow({ includeEmpty: false }, async (row, rowNumber) => {
+      if (rowNumber === 1) return; 
+
+      const values = row.values as any[];
+      const email = values[1];
+      const project = values[2];
+      const period = Number(values[3]);
+      const workAgain = values[4];
+      const grade = values[5];
+      const weakPoint = values[6];
+      const strengthPoint = values[7];
+
+      const avaliadoUser = await this.usersService.findByEmail(email);
+
+      const mappedWorkAgain = motivacaoMap[String(workAgain).trim()];
+
+      const avaliacao360Data = {
+        idAvaliador: userId,   
+        idAvaliado: avaliadoUser.id,
+        idCiclo: cicleId,       
+        nota: grade,          
+        pontosFortes: strengthPoint,  
+        pontosMelhora: weakPoint, 
+        nomeProjeto: project,   
+        periodoMeses: period,  
+        trabalhariaNovamente: mappedWorkAgain,
+      };
+  
+      await this.avaliacao360Service.create360(avaliacao360Data);
+      
+    });
   }
 
   async importReferencias(sheet: ExcelJS.Worksheet, userId: number, cicleId: number) {
@@ -99,12 +143,11 @@ export class ExcelService {
       const email = values[1];
       const justificativa = values[2];
       
-      console.log(email);
-      const existingUser = await this.usersService.findByEmail(email);
+      const referenciadoUser = await this.usersService.findByEmail(email);
 
       const referenciaData = {
         idReferenciador: userId,
-        idReferenciado: existingUser.id,
+        idReferenciado: referenciadoUser.id,
         justificativa: justificativa,
         idCiclo: cicleId
       };
